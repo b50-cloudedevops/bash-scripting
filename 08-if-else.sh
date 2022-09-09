d7bb95fbc69a61478b9215f00034a1cd1898c517
vamsi434 <busavamsi@gmail.com>
2022-09-09 10:15:03 +0530

    asz

--- 08-if-else.sh
+++ 08-if-else.sh
@@ -6,8 +6,8 @@
 ACTION=$1
 if [ "$ACTION" = "start" ] ; then
  echo "selected choice is start"
- elif [ "$ACTION" = "stop"] ; then
- echo "selected choice is stop"
+elif [ "$ACTION" = "stop" ] ; then
+  echo "selected choice is stop"
 else 
  echo "only valid option is start"
 fi
