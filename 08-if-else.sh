f0d0154ec515387c81578e0bffce6a95bf627d98
vamsi434 <busavamsi@gmail.com>
2022-09-09 10:46:28 +0530

    ased

--- 08-if-else.sh
+++ 08-if-else.sh
@@ -4,16 +4,16 @@
 #[-n "$var"]
 
 ACTION=$1
-if [ "$ACTION" = "start" ] ; then
- echo "selected choice is start"
- exit 2
-elif [ "$ACTION" = "stop" ] ; then
+#if [ "$ACTION" = "start" ] ; then
+# echo "selected choice is start"
+# exit 2
+#elif [ "$ACTION" = "stop" ] ; then
   echo "selected choice is stop"
   exit 1
-else 
- echo "only valid option is start"
- exit 2
-fi
+#else 
+# echo "only valid option is start"
+# exit 2
+#fi
 
 #-z will be true, if the supplied input is null
 if [-z $ACTION ]; then
