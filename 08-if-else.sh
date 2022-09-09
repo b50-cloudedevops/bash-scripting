7e769f2f7c030e939b3f17720584364d04ecced8
vamsi434 <busavamsi@gmail.com>
2022-09-09 10:23:45 +0530

    lks

--- 08-if-else.sh
+++ 08-if-else.sh
@@ -6,8 +6,11 @@
 ACTION=$1
 if [ "$ACTION" = "start" ] ; then
  echo "selected choice is start"
+ exit 2
 elif [ "$ACTION" = "stop" ] ; then
   echo "selected choice is stop"
+  exit 1
 else 
  echo "only valid option is start"
+ exit 2
 fi
