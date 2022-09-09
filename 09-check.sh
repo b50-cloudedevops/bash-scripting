451a50d9ee5e2f87754ec768c8140679426b8dba
vamsi434 <busavamsi@gmail.com>
2022-09-09 11:01:30 +0530

    ajsbr

--- 09-check.sh
+++ 09-check.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ID=$(id -u)
-if [ $ID -eq: 0 ]; then 
- echo "Exceuting httpd installation"
- yum install httpd -y
+if [ "$ID" -eq "0" ]; then 
+  echo "Exceuting httpd installation"
+  yum install httpd -y
+
 else 
- echo -e "try Executing the script with sudo or a root user"
- exit 1
+  echo -e "try Executing the script with sudo or a root user"
+  exit 1
+
 fi
