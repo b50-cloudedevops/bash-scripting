9e257dc6a6d4733e6001872e10e56474b34df75d
vamsi434 <busavamsi@gmail.com>
2022-09-11 20:59:36 +0530

    as

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -6,7 +6,7 @@ echo -n "configuring mongodb repo: "
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
 stat $?
 echo -n "Installing the ${COMPONENT}: "
-yum install -y mongodb-org 
+yum install -y mongodb-org >> /tmp/${COMPONENT}.log
 stat $?
 echo -n "Updating the $COMPONENT configuration:"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
