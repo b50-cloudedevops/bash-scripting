513f66e54f0ca4e226f0c4d6752323095efc8c51
vamsi434 <busavamsi@gmail.com>
2022-09-11 20:38:53 +0530

    added

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -13,11 +13,16 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 stat $?
 cd /usr/share/nginx/html
 rm -rf *
+echo -n "extracting the zip file"
 unzip -o /tmp/frontend.zip  >>  /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
+echo -n "performing cleanup: "
 rm -rf frontend-main README.md
+stat $?
+echo -n "configuring the reverse proxy"
 mv localhost.conf  /etc/nginx/default.d/roboshop.conf 
 systemctl restart nginx
-echo "staring the nginx"
+echo -n "staring the nginx"
 stat $?
+
