c3b4fc569405cd662591adb0b062e61cb00a635d
vamsi434 <busavamsi@gmail.com>
2022-09-11 14:09:31 +0530

    asg

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e # ensures your script will stop if any of the instruction fails
 source components/common.sh
-
+echo "Installing nginx"
 yum install nginx -y >> /tmp/frontend.log
 systemctl enable nginx
+echo "starting nginx"
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip /tmp/frontend.log
+unzip /tmp/frontend.zip  >>  /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf  /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
