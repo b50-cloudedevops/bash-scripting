e5e807b3b8651af19228633f8228bcae2a17c082
vamsi434 <busavamsi@gmail.com>
2022-09-11 14:32:57 +0530

    cle

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -3,3 +3,12 @@ if [ "$ID" -ne "0" ]; then
   echo "Try to execute the script with sudo user or root user"
   exit 1
 fi
+
+
+stat() {
+if [ $? -eq 0 ]; then
+  echo -e "Success"
+else
+ echo -e "failure, look for the logs"
+fi
+}
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,37 +3,21 @@ set -e # ensures your script will stop if any of the instruction fails
 source components/common.sh
 echo -n "Installing nginx: "
 yum install nginx -y >> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-  echo -e "Success"
-else
- echo -e "failure, look for the logs"
-fi
+stat $?
 systemctl enable nginx
 echo -n "starting nginx: "
 systemctl start nginx
-if [ $? -eq 0 ]; then
-  echo -e "Success"
-else
- echo -e "failure, look for the logs"
-fi
-
+stat $?
 echo -n "Downloading the code: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "Success"
-else
- echo -e "failure, look for the logs"
-fi
+stat $?
 cd /usr/share/nginx/html
 rm -rf *
 unzip -o /tmp/frontend.zip  >>  /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
-mv localhost.conf  /etc/nginx/default.d/roboshop.conf
-if [ $? -eq 0 ]; then
-  echo -e "Success"
-else
- echo -e "failure, look for the logs"
-fi
+mv localhost.conf  /etc/nginx/default.d/roboshop.conf 
 systemctl restart nginx
+echo "staring the nginx"
+stat $?
