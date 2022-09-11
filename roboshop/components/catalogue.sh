871656f2f6f850d490a8455ee7d8e14c7050aebf
vamsi434 <busavamsi@gmail.com>
2022-09-11 21:30:08 +0530

    lkd

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+source components/common.sh
+COMPONENT= catalogue
+FUSER=roboshop
+echo -n " Configuring Yum repos for nodejs: "
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+stat $?
+echo -n "Installing nodejs: "
+yum install nodejs -y >> /tmp/${COMPONENT}.log
+echo -n " Adding ${FUSER} user: "
+useradd ${FUSER}
+stat $?
+echo -n "Downloading $COMPONENT: "
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
 
-ID=$(id -u)
-if [ "$ID" -ne "0" ]; then 
-  echo "Try to execute the script with sudo user or root user"
-  exit 1
-fi
+echo -n "CLeanup of old ${COMPONENT} content: "
+rm -rf /home/${FUSER}/${COMPONENT} >> /tmp/${COMPONENT}.log
+echo -n "Extracting $COMPONENT content: "
+cd /home/${FUSER}
+unzip -o /tmp/${FUSER}.zip >> /tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log
+stat $?
+cd /home/roboshop/catalogue
+echo -n "Installing $COMPONENT Dependencies: "
+npm install >> /tmp/${COMPONENT}.log
+stat $?
