97248a76010751ab5d3c3839f6765c5a062ebfa0
vamsi434 <busavamsi@gmail.com>
2022-09-11 12:39:46 +0530

    azs

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,12 +5,10 @@ if [ "$ID" -ne "0" ]; then
   echo "Try to execute the script with sudo user or root user"
   exit 1
 fi
-
 yum install nginx -y
 systmectl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
