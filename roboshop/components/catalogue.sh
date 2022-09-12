a135197fa9f3ceddc83143b54199328a014fdf98
vamsi434 <busavamsi@gmail.com>
2022-09-12 10:30:39 +0530

    ihc

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -11,23 +11,24 @@ yum install nodejs -y >> /tmp/${COMPONENT}.log
 stat $?
 
 echo -n " Adding ${FUSER} user: "
-id ${FUSER} || useradd ${roboshop}
+id ${FUSER} || useradd ${FUSER}
 stat $?
 
 echo -n "Downloading $COMPONENT: "
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
+echo -n "Clean up of old $COMPONENT content:"
+rm -rf /home/${FUSER}/${COMPONENT} >> /tmp/${COMPONENT}.log
+stat $?
 
 echo -n "Extracting $COMPONENT content: "
-cd /home/${FUSER} 
+cd /home/${FUSER}
 unzip -o /tmp/${COMPONENT}.zip >> /tmp/${COMPONENT}.log  &&  mv ${COMPONENT}-main ${COMPONENT}  >>  /tmp/${COMPONENT}.log
 stat $?
 
 echo -n "Changing the ownership to $FUSER: "
 chown -R $FUSER:$FUSER $COMPONENT/
-
-
 
 echo -n "Installing $COMPONENT Dependencies: "
 cd $COMPONENT && npm install >> /tmp/${COMPONENT}.log
