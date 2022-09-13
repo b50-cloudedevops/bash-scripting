75e0fe98894d7ebbbb600c080646eadeae779716
vamsi434 <busavamsi@gmail.com>
2022-09-13 10:08:33 +0530

    awsl

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -9,17 +9,15 @@ stat $?
 echo -n "Installing $COMPONENT-:  "
 yum install redis-6.2.7 -y >> /tmp/${COMPONENT}.log
 stat $?
+
+echo -n "Whitelisting the redis config: "
+sed -i -e 's/127.0.0.01/0.0.0.0/g' /etc/redis.conf
+stat $?
 # vim /etc/redis.conf
 # vim /etc/redis/redis.conf
 
-echo -n "enabling the ${COMPONENT}: "
-systemctl enable redis
-stat $?
-
 echo -n "Starting the ${COMPONENT}: "
+systemctl enable redis
 systemctl start redis
-stat $?
-
-echo -n "Checking the ${COMPONENT} status: "
 systemctl status redis -l
 stat $?
