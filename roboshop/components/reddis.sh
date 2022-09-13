6b5e2f2933b783e91b09e2fe99fe71c1a53ec4af
vamsi434 <busavamsi@gmail.com>
2022-09-13 09:36:10 +0530

    po

--- roboshop/components/reddis.sh
+++ roboshop/components/reddis.sh
@@ -1,5 +1,16 @@
-ID=$(id -u)
-if [ "$ID" -ne "0" ]; then 
-  echo "Try to execute the script with sudo user or root user"
-  exit 1
-fi
+#!/bin/bash
+set -e
+source components/common.sh
+COMPONENT=redis
+echo -n "Configuring the $COMPONENT repo:"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+stat $?
+
+echo -n "Installing $COMPONENT"
+yum install redis-6.2.7 -y >> /tmp/${COMPONENT}.log
+stat $?
+# vim /etc/redis.conf
+# vim /etc/redis/redis.conf
+# systemctl enable redis
+# systemctl start redis
+# systemctl status redis -l
