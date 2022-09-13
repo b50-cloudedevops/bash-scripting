4dbfc60f6829b3ebe29d227da410146259c018af
vamsi434 <busavamsi@gmail.com>
2022-09-13 10:04:26 +0530

    ed

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -2,15 +2,24 @@
 set -e
 source components/common.sh
 COMPONENT=redis
-echo -n "Configuring the $COMPONENT repo:"
+echo -n "Configuring the $COMPONENT repo: "
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 stat $?
 
-echo -n "Installing $COMPONENT"
+echo -n "Installing $COMPONENT-:  "
 yum install redis-6.2.7 -y >> /tmp/${COMPONENT}.log
 stat $?
 # vim /etc/redis.conf
 # vim /etc/redis/redis.conf
-# systemctl enable redis
-# systemctl start redis
-# systemctl status redis -l
+
+echo -n "enabling the ${COMPONENT}: "
+systemctl enable redis
+stat $?
+
+echo -n "Starting the ${COMPONENT}: "
+systemctl start redis
+stat $?
+
+echo -n "Checking the ${COMPONENT} status: "
+systemctl status redis -l
+stat $?
