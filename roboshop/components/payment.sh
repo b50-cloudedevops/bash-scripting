8a2804776fca761bb9fe7c3832c3472d9432cb9e
vamsi434 <vamsibusa@gmail.com>
2022-09-14 21:47:58 +0530

    thsg

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+set -e
+source components/common.sh
+COMPONENT=payment
+NODEJS
 
-ID=$(id -u)
-if [ "$ID" -ne "0" ]; then 
-  echo "Try to execute the script with sudo user or root user"
-  exit 1
-fi
