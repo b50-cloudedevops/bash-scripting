477c3f2d3ef6f3315301ce838db9bf2c81abc3f5
vamsi434 <vamsibusa@gmail.com>
2022-09-14 21:23:19 +0530

    osdh

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -5,16 +5,16 @@ COMPONENT=rabbitmq
 
 
 echo -n "Installing Erlnad dependency: "
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE
+yum install -o  https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE
 stat $?
 
 echo -n "Configuring the yum repo for $COMPONENT"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
+curl -s -o https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
 echo -n "Installing RabbitMq: "
 yum install rabbitmq-server -y >> $LOGFILE
-stat $?
+stat $? 
 
 echo -n "starting $COMPONENT"
 systemctl enable rabbitmq-server >> $LOGFILE
