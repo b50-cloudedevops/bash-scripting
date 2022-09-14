bcb2d56a70eba30ac950a4c337b5ccbc7bb0c300
vamsi434 <vamsibusa@gmail.com>
2022-09-14 21:30:49 +0530

    thsg

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -3,11 +3,8 @@ source components/common.sh
 COMPONENT=rabbitmq
 
 
-echo -n "Installing Erlnad dependency: "
-yum install -o  https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE
-stat $?
-
-echo -n "Configuring the yum repo for $COMPONENT"
+echo -n "Installing  and configuring Erlnad dependency: "
+yum install  https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE
 curl -s -o https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
@@ -20,11 +17,12 @@ systemctl enable rabbitmq-server >> $LOGFILE
 systemctl start rabbitmq-server >> $LOGFILE
 stat $?
 
-
-echo -n "Creating $COMPONENT Application user: "
-rabbitmqctl add_user roboshop roboshop123
-stat $?
-
+rabbitmqctl list_users | grep roboshop  &>> ${LOGFILE}
+if [ $? -eq 0 ]; then
+ echo -n "Creating $COMPONENT Application user: "
+ rabbitmqctl add_user roboshop roboshop123
+ stat $?
+fi
 echo -n "Configuring the $COMPONENT $FUSER permissions: "
 rabbitmqctl set_user_tags roboshop administrator &>> ${LOGFILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 stat $?
