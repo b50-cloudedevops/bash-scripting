945aa73ffb359e477e37ee5a137623e4d1d03db3
vamsi434 <vamsibusa@gmail.com>
2022-09-14 22:26:37 +0530

    thdg

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -83,4 +83,30 @@ mv target/$COMPONENT-1.0.jar $COMPONENT.jar
 stat $?
 
 CONFIG_SVC
+
+echo -e "\n *********$COMPONENT Installation completed*********************\n"
+
+}
+PYTHON() {
+
+  echo -n "Installing python!: "
+yum install python36 gcc python3-devel -y >> ${LOGFILE}
+stat $?
+
+USER_SETUP
+
+DOWNLOAD_AND_EXTRACT
+
+cd /home/${FUSER}/${COMPONENT}
+pip3 install -r requirements.txt >> ${LOGFILE}
+stat $?
+
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+
+echo -n "Updating the $COMPONENT file: "
+sed -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}" payment.ini 
+stat $?
+CONFIG_SVC
+
 }
