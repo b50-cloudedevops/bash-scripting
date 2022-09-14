b257e1d3e7de3091b3b6ff1864f0947a70f106f2
vamsi434 <vamsibusa@gmail.com>
2022-09-14 21:57:43 +0530

    thdgv

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -6,7 +6,11 @@ COMPONENT=payment
 echo -n "Installing python!: "
 yum install python36 gcc python3-devel -y >> ${LOGFILE}
 stat $?
+
 USER_SETUP
+
 DOWNLOAD_AND_EXTRACT
+
 cd /home/${FUSER}/${COMPONENT}
 pip3 install -r requirements.txt >> ${LOGFILE}
+stat $?
