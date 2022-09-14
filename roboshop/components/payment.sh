f7a92adb677e9c249613a7b2194d250872bc791f
vamsi434 <vamsibusa@gmail.com>
2022-09-14 22:14:53 +0530

    thsgdv

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -14,3 +14,11 @@ DOWNLOAD_AND_EXTRACT
 cd /home/${FUSER}/${COMPONENT}
 pip3 install -r requirements.txt >> ${LOGFILE}
 stat $?
+
+USER_ID=${id -u roboshop}
+GROUP_ID=${id -g roboshop}
+
+echo -n "Updating the $COMPONENT file: "
+sed -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}" payment.ini 
+stat $?
+
