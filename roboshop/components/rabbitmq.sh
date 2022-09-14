363924f0e0c36dcaa5c08fca28814a5c26278236
vamsi434 <vamsibusa@gmail.com>
2022-09-14 21:18:18 +0530

    rbbt

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
+set -e
+source components/common.sh
+COMPONENT=rabbitmq
 
-ID=$(id -u)
-if [ "$ID" -ne "0" ]; then 
-  echo "Try to execute the script with sudo user or root user"
-  exit 1
-fi
+
+echo -n "Installing Erlnad dependency: "
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE
+stat $?
+
+echo -n "Configuring the yum repo for $COMPONENT"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+stat $?
+
+echo -n "Installing RabbitMq: "
+yum install rabbitmq-server -y >> $LOGFILE
+stat $?
+
+echo -n "starting $COMPONENT"
+systemctl enable rabbitmq-server >> $LOGFILE
+systemctl start rabbitmq-server >> $LOGFILE
+stat $?
+
+
+echo -n "Creating $COMPONENT Application user: "
+rabbitmqctl add_user roboshop roboshop123
+stat $?
+
+echo -n "Configuring the $COMPONENT $FUSER permissions: "
+rabbitmqctl set_user_tags roboshop administrator >> $LOGFILE && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+stat $?
+
