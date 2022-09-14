9510a7092d41f600692b66a8b69ef3cb5c2612b6
vamsi434 <vamsibusa@gmail.com>
2022-09-14 18:17:52 +0530

    ask

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 source components/common.sh
 
 COMPONENT=mysql
@@ -26,10 +24,28 @@ echo -n "fetching the default root password: "
 DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
 stat $?
 
-echo -n "reset root  password: "
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" &>> ${LOGFILE}
+echo show databases | mysql -uroot -pRoboShop@1 | &>> ${LOGFILE}
+if [ $? -ne 0 ]; then
+ echo -n "reset root  password: "
+ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" &>> ${LOGFILE}
+ stat $?
+fi 
+
+echo 'show plugins;' | mysql -uroot -pRoboShop@1 &>> ${LOGFILE} | grep validate_password > ${LOGFILE}
+if [ $? -eq 0 ] ; then 
+ echo -n "Uninstall the password validate plugin: "
+ echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 
+ stat $?
+fi
+
+echo "downloading the schema:"
+cd /tmp
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip" &>> ${LOGFILE} && unzip -o /tmp/mysql.zip
 stat $?
 
-echo -n "Uninstall the password validate plugin: "
-echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
+echo -n "Load schema: "
+cd /tmp/mysql-main/
+mysql -uroot -pRoboShop@1 < shipping.sql &>> ${LOGFILE}
 stat $?
+
+echo "***********$COMPONENT Installation completed"
