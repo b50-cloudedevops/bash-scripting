32306c24868ef261869e67508f4003a462e283b8
vamsi434 <vamsibusa@gmail.com>
2022-09-17 13:16:04 +0530

    eghk

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # AMI_ID=ami-00ff427d936335825
+if [-z "$1" ]; then
+ echo -n "Input machine name is missing: "
+ exit 1
+fi 
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "AMI Id which is fetched $AMI_ID"
