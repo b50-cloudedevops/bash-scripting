290d4ee25d0e120ee40f0517f90ee5089bc2264c
vamsi434 <vamsibusa@gmail.com>
2022-09-17 12:47:56 +0530

    thsb

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # AMI_ID=ami-00ff427d936335825
 AMI_ID =$(aws ec2 describe-images --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
-echo "AMI Id which is fetched: $AMI_ID"
+echo "AMI Id which is fetched $AMI_ID"
