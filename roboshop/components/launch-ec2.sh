c6d2882fd09d4e56aec8aa2399bc13ea2ae1f659
vamsi434 <vamsibusa@gmail.com>
2022-09-17 13:02:49 +0530

    cvb

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # AMI_ID=ami-00ff427d936335825
+COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "AMI Id which is fetched $AMI_ID"
+SGID="sg-0c45f649a0dc57860"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=Instance, Tags=[{Key=Name, Value=${COMPONENT}}]" --security-group-ids ${SGID}
