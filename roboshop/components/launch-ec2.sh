0e73fa793691cce75444d99dc54820f22e02007a
vamsi434 <vamsibusa@gmail.com>
2022-09-18 10:02:04 +0530

    wsn

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -4,19 +4,22 @@ if [ -z  "$1" ]; then
  echo -e  "Input machine name is missing: "
  exit 1
 fi 
+
 COMPONENT=$1
+ZONEID="Z0175006XAWHYA3OXLXM"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name, Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "AMI Id which is fetched $AMI_ID"
 SGID="sg-0c45f649a0dc57860"
 
+echo "The AMI which we are using is $AMI_ID"
 create-server() {
   PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
   echo "Private IP of created machine is $PRIVATE_IP"
   echo "Spot Instance $COMPONENT is ready: "
   echo "Creating Route53 Record........"
 
-  sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json>/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0175006XAWHYA3OXLXM --change-batch file:///tmp/record.json | jq
+  sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file:///tmp/record.json | jq
 
 }
 
